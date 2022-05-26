f5d7f80b7c2b9842af88c1024c78cc79e77f5dab
nyoarchive <thomasdjossou@gmail.com>
2022-05-26 00:58:40 -0400

    Use notes instead of "1" and "0"
    I wanted to use this algorithm in Tone.js with a Sequence where each sequence is played by a different instrument. getPattern now takes a note argument and returns the generated pattern with the note and null (which afaik is how Tone.js reads rests).

--- src/index.js
+++ src/index.js
@@ -7,34 +7,36 @@
    *  @param {Number} pulses Number of pulses in the pattern
    *  @param {Number} steps  Number of steps in the pattern (pattern length)
    */
-const getPattern = (pulses, steps) => {
+const getPattern(pulses, steps, note) {
+  //retrieved from https://github.com/mkontogiannis/euclidean-rhythms;
+  // edited to return notes
   if (pulses < 0 || steps < 0 || steps < pulses) {
-  	return [];
+    return [];
   }
 
   // Create the two arrays
-  let first = new Array(pulses).fill([1]);
-  let second = new Array(steps - pulses).fill([0]);
+  var first = new Array(pulses).fill([note]);
+  var second = new Array(steps - pulses).fill([null]);
 
-  let firstLength = first.length;
-  let minLength = Math.min(firstLength, second.length);
+  var firstLength = first.length;
+  var minLength = Math.min(firstLength, second.length);
 
-  let loopThreshold = 0;
+  var loopThreshold = 0;
   // Loop until at least one array has length gt 2 (1 for first loop)
   while (minLength > loopThreshold) {
-  	// Allow only loopThreshold to be zero on the first loop
-  	if (loopThreshold === 0) {
-  		loopThreshold = 1;
-  	}
+    // Allow only loopThreshold to be zero on the first loop
+    if (loopThreshold === 0) {
+      loopThreshold = 1;
+    }
 
-  	// For the minimum array loop and concat
+    // For the minimum array loop and concat
     for (var x = 0; x < minLength; x++) {
       first[x] = Array.prototype.concat.call(first[x], second[x]);
     }
 
     // if the second was the bigger array, slice the remaining elements/arrays and update
     if (minLength === firstLength) {
-    	second = Array.prototype.slice.call(second, minLength);
+      second = Array.prototype.slice.call(second, minLength);
     }
     // Otherwise update the second (smallest array) with the remainders of the first
     // and update the first array to include onlt the extended sub-arrays
@@ -44,19 +46,20 @@ const getPattern = (pulses, steps) => {
     }
     firstLength = first.length;
     minLength = Math.min(firstLength, second.length);
-	}
+  }
 
-	// Build the final array
-  let pattern = [];
-  first.forEach(f => {
+  // Build the final array
+  var pattern = [];
+  first.forEach(function(f) {
     pattern = Array.prototype.concat.call(pattern, f);
   });
-  second.forEach(s => {
+  second.forEach(function(s) {
     pattern = Array.prototype.concat.call(pattern, s);
   });
 
   return pattern;
-};
+}
+
 
 module.exports = {
   getPattern: getPattern
